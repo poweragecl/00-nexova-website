56ef0d2dcadb88fee1c3ce8fd36d9182c570e0ef
poweragecl <hexed@hotmail.cl>
2026-05-23 20:20:59 -0400

    feat: agregado componente navbar y menu mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { Routes, Route } from 'react-router-dom'
-import Home      from './pages/Home/Home'
-import Nosotros  from './pages/Nosotros/Nosotros'
-import Servicios from './pages/Servicios/Servicios'
+import Navbar     from './components/Navbar/Navbar'
+import Home       from './pages/Home/Home'
+import Nosotros   from './pages/Nosotros/Nosotros'
+import Servicios  from './pages/Servicios/Servicios'
 import Portafolio from './pages/Portafolio/Portafolio'
-import Contacto  from './pages/Contacto/Contacto'
+import Contacto   from './pages/Contacto/Contacto'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/"           element={<Home />} />
-      <Route path="/nosotros"   element={<Nosotros />} />
-      <Route path="/servicios"  element={<Servicios />} />
-      <Route path="/portafolio" element={<Portafolio />} />
-      <Route path="/contacto"   element={<Contacto />} />
-    </Routes>
+    <>
+      <Navbar />
+      <main style={{ paddingTop: '68px' }}>
+        <Routes>
+          <Route path="/"           element={<Home />} />
+          <Route path="/nosotros"   element={<Nosotros />} />
+          <Route path="/servicios"  element={<Servicios />} />
+          <Route path="/portafolio" element={<Portafolio />} />
+          <Route path="/contacto"   element={<Contacto />} />
+        </Routes>
+      </main>
+    </>
   )
 }
 
